ca9f2910e5b841c640df207ceff06914110549a0
Jan Johannes <jan@ntr.io>
2011-12-29 21:03:19 +0100

    Additional export to be used by couchDAV module

--- src/couchdb/couch_httpd_db.erl
+++ src/couchdb/couch_httpd_db.erl
@@ -14,7 +14,7 @@
 -include("couch_db.hrl").
 
 -export([handle_request/1, handle_compact_req/2, handle_design_req/2,
-    db_req/2, couch_doc_open/4,handle_changes_req/2,
+    db_req/2, db_doc_req/3, couch_doc_open/4,handle_changes_req/2,
     update_doc_result_to_json/1, update_doc_result_to_json/2,
     handle_design_info_req/3]).
 
